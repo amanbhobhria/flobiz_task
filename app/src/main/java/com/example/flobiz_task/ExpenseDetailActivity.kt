2059c339e53b657cc5a3eb27a74ba353879f0926
amanbhobhria <75074918+amanbhobhria@users.noreply.github.com>
2024-12-04 15:38:52 +0530

    UI Design Complete of  EditExpense and ExpenseDetail Screen
    UI Complete of all screens in xml.

---
+++ app/src/main/java/com/example/flobiz_task/ExpenseDetailActivity.kt
@@ -0,0 +1,19 @@
+package com.example.flobiz_task
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+
+class ExpenseDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense_detail)
+
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorSearch)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+    }
+}
--- app/src/main/java/com/example/flobiz_task/MainActivity.kt
+++ app/src/main/java/com/example/flobiz_task/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.flobiz_task
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.example.flobiz_task.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binidng:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        window.statusBarColor = Color.WHITE
+        binidng = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binidng.root)
+
+        binidng.addNewBtn.setOnClickListener{
+            val intent = Intent(this, AddNewTransctionActivity::class.java)
+            startActivity(intent)
+        }
+
+        binidng.menuSettings.setOnClickListener {
+            val intent = Intent(this, ExpenseDetailActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorSearch)
+
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
 
