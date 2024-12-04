30a43ea47285a5117a590bb302ffd33808625747
amanbhobhria <75074918+amanbhobhria@users.noreply.github.com>
2024-12-04 10:55:06 +0530

    DashBoard Screen UI
    Dasboard screen ui completed
    By adding searchbar,recylerview for transctions,floating action button to add new transction,bottom navigation button

--- app/src/main/java/com/example/flobiz_task/MainActivity.kt
+++ app/src/main/java/com/example/flobiz_task/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.flobiz_task
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = Color.WHITE
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+
     }
 }
