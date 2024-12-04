7f04abb9a6cb7479e8f17c82615291ae059dd35b
amanbhobhria <75074918+amanbhobhria@users.noreply.github.com>
2024-12-04 06:35:41 +0530

    Project Created
    Initial Setup

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "flobiz_task"
+include(":app")
+ 
