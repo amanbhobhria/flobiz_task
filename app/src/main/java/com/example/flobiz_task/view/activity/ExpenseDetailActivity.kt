ae0a9a4e830923e18a0415f9a133a7ecfda1e00e
amanbhobhria <75074918+amanbhobhria@users.noreply.github.com>
2024-12-08 07:08:43 +0530

    Bug fixes and performance improvement

--- app/src/main/java/com/example/flobiz_task/view/activity/ExpenseDetailActivity.kt
+++ app/src/main/java/com/example/flobiz_task/view/activity/ExpenseDetailActivity.kt
@@ -3,6 +3,7 @@ package com.example.flobiz_task.view.activity
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -12,7 +13,11 @@ import androidx.lifecycle.Observer
 import com.example.flobiz_task.databinding.ActivityExpenseDetailBinding
 import com.example.flobiz_task.model.data.Expense
 import com.example.flobiz_task.viewmodel.ExpenseDetailViewModel
+import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @AndroidEntryPoint
 class ExpenseDetailActivity : AppCompatActivity() {
@@ -38,6 +43,7 @@ class ExpenseDetailActivity : AppCompatActivity() {
         binding.saveBtn.setOnClickListener { saveExpense() }
         binding.deleteBtn.setOnClickListener { confirmDeletion() }
         binding.backIcon.setOnClickListener { finish() }
+        binding.calendarBtn.setOnClickListener { showDatePicker() }
 
 
 
@@ -61,11 +67,25 @@ class ExpenseDetailActivity : AppCompatActivity() {
 
     private fun observeViewModel() {
         viewModel.isEditable.observe(this, Observer { editable ->
-            binding.dateEdtTxt.isEnabled = editable
-            binding.dateEdtTxt.isFocusableInTouchMode = editable
+
 
             binding.descEdtTxt.isEnabled = editable
             binding.descEdtTxt.isFocusableInTouchMode = editable
+
+
+
+            if (editable==true) {
+                binding.descLyt.setOnClickListener{
+                    binding.descEdtTxt.requestFocus()
+                    binding.descEdtTxt.setSelection(binding.descEdtTxt.text.length)
+                }
+
+                binding.calendarBtn.visibility=View.VISIBLE
+
+            }
+            else {
+                binding.calendarBtn.visibility = View.GONE
+            }
 
             binding.amountEdtTxt.isEnabled = editable
             binding.amountEdtTxt.isFocusableInTouchMode = editable
@@ -99,6 +119,7 @@ class ExpenseDetailActivity : AppCompatActivity() {
         )
 
         viewModel.updateExpense(expenseId!!, updatedExpense)
+//        viewModel.setEditable(false)
     }
 
     private fun confirmDeletion() {
@@ -111,6 +132,46 @@ class ExpenseDetailActivity : AppCompatActivity() {
             .setNegativeButton("No", null)
             .show()
     }
+
+
+    private fun showDatePicker() {
+
+        val dateInMillis = try {
+            val formatter = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+            val date = formatter.parse(binding.dateEdtTxt.text.toString())
+            Log.d("ExpenseDetail", "Date = $date")
+            date?.time
+        } catch (e: Exception) {
+            null
+        }
+
+
+
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select Date")
+            .apply {
+                dateInMillis?.let {
+                    setSelection(it)
+                }
+            }
+            .build()
+
+
+
+        datePicker.show(supportFragmentManager, "DATE_PICKER")
+
+
+
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            val formatter = SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault())
+            val selectedDate = formatter.format(Date(selection))
+            binding.dateEdtTxt.setText(selectedDate)
+        }
+    }
+
+
+
+
 }
 
 
